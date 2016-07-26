981b8e53599c9515f136b7545c03201b25176062
Arnaud DM <arnaud.dematteis@icloud.com>
2016-07-26 22:45:40 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ping-pong
+//
+//  Created by Arnaud DE MATTEIS on 22/07/2016.
+//  Copyright © 2016 Arnaud DE MATTEIS. All rights reserved.
+//
+//  Developped with XCode on a
+//  MacBookPro with 16Gb of RAM
+//
+
+#include <cstdlib>
+#include <iostream>
+#include <unistd.h>
+
+int main(int argc, char const *argv[]) {
+    return 0;
+}
