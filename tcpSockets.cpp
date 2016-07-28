98343e8d8c58c8861efe7b6d8a682ccc48d5f0c4
Arnaud DM <arnaud.dematteis@icloud.com>
2016-07-28 21:30:50 +0200

    Adding files of TCP sockets

---
+++ tcpSockets.cpp
@@ -0,0 +1,9 @@
+//
+//  tcpSockets.cpp
+//  ping-pong
+//
+//  Created by Arnaud DE MATTEIS on 28/07/2016.
+//  Copyright © 2016 Arnaud DE MATTEIS. All rights reserved.
+//
+
+#include "tcpSockets.hpp"
---
+++ tcpSockets.hpp
@@ -0,0 +1,18 @@
+//
+//  tcpSockets.hpp
+//  ping-pong
+//
+//  Created by Arnaud DE MATTEIS on 28/07/2016.
+//  Copyright © 2016 Arnaud DE MATTEIS. All rights reserved.
+//
+
+#ifndef tcpSockets_hpp
+#define tcpSockets_hpp
+
+#include <stdio.h>
+
+class tcpSockets {
+    
+};
+
+#endif /* tcpSockets_hpp */
